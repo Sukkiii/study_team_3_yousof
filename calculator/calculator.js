7d67f4b3f52ff9b2d90495d0b32b19f9bee637b3
sukki <knk951213@naver.com>
2023-10-04 21:49:23 +0900

    feature: plusminus func add

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,29 +1,35 @@
 const buttons = document.querySelectorAll('button')
 const valueReturn = document.querySelector('input')
 
+let currentValue = '0'
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const buttonText = button.textContent;
     let currentValue = valueReturn.value;
 
     switch (button.dataset.type) {
-      case "operator":
+      case 'operator':
       currentValue += buttonText;
       break;
-      case "ac":
-        currentValue = "0";
+      case 'ac':
+        currentValue = '0';
         break;
-      case "plus-minus":
+      case 'plus-minus':
         currentValue = currentValue * -1;
         break;
-      case "percent":
+      case 'percent':
         currentValue = `${currentValue * 0.01}%`;
         break;
-      case "equals":
-        console.log("equals");
+      case 'equals':
+        try {
+          currentValue = eval(currentValue);
+        } catch (error) {
+          currentValue = 'error';
+        }
         break;
       default:
-        if (currentValue === "0") {
+        if (currentValue === '0' || currentValue === 'error') {
           currentValue = buttonText;
         } else currentValue += buttonText;
     }
